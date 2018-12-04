c127cda1f282e9db47a66926a59fc9ab06e29535
Mika Katua <44744529+mikakatua@users.noreply.github.com>
2018-12-04 17:48:13 +0100

    Create user_demo.sh

---
+++ labs/user_demo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Create groups with specific GID
+groupadd -g 5000 developer
+groupadd -g 5001 dba
+
+# Create a system group (lower GID)
+groupadd -r backup
+
+# Create a user with supplementary groups
+# A group with the same name as the user will also be created
+useradd -c "Waldo Developer" -G developer waldo
+
+# Set the user password. Read it from STDIN
+echo secret | passwd waldo --stdin
+
+# Set password expiration in 120 days
+chage -M 120 waldo
+
+# To list password aging use
+# chage -l waldo
+
+# Create a system user (lower UID) without login shell
+useradd -c "Operator" -r -g backup -G dba -M -d / -s /bin/nologin sysop
+
+# Create a user with specific UID and the home in /opt
+# A group with the same name as the user will also be created
+useradd -c "Power User" -u 2001 -G developer,dba -b /opt super
+
+# Set the user password. Read it from STDIN
+echo p0w3rU | passwd super --stdin
